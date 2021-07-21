2206acce9ad15600e44617acd23ccc700205b6d3
kashifAli96 <kashiftauseef@gmail.com>
2021-07-21 15:12:39 +0100

    patch block request added

--- notion.js
+++ notion.js
@@ -14,31 +14,60 @@ async function getBlock(blockID) {
 async function getTitle(id) {
   const response = await notion.databases.query({
     database_id: id,
-    // filter: {
-    //   or: [
-    //     {
-    //       property: "In stock",
-    //       checkbox: {
-    //         equals: true,
-    //       },
-    //     },
-    //     {
-    //       property: "Cost of next trip",
-    //       number: {
-    //         greater_than_or_equal_to: 2,
-    //       },
-    //     },
-    //   ],
-    // },
-    // sorts: [
-    //   {
-    //     property: "Last ordered",
-    //     direction: "ascending",
-    //   },
-    // ],
   });
-  console.log(" here ", response);
+  console.log(" TITLE ", response);
   return response;
 }
 
-module.exports = { getBlock, getTitle };
+async function addDataBlock(blockID, data, content) {
+  const response = await notion.blocks.children.append({
+    block_id: blockID,
+    children: [
+      {
+        object: "block",
+        type: "heading_2",
+        heading_2: {
+          text: [
+            {
+              type: "text",
+              text: {
+                content: data,
+              },
+            },
+          ],
+        },
+      },
+      {
+        object: "block",
+        type: "paragraph",
+        paragraph: {
+          text: [
+            {
+              type: "text",
+              text: {
+                content: content,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+  console.log(response);
+}
+
+async function getAPI() {
+  var axios = require("axios");
+
+  try {
+    const response = await axios.get(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log("error");
+    return "error";
+  }
+}
+
+module.exports = { getBlock, getTitle, addDataBlock, getAPI };
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
 const express = require("express");
-let ejs = require("ejs");
+// let ejs = require("ejs");
 const app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
 const notionjs = require("./notion");
+const moment = require("moment");
 
 app.get("/", (req, res) => {
   try {
     (async () => {
       var title = await notionjs.getTitle(process.env.NOTION_DB);
       var json = await notionjs.getBlock(process.env.blockID);
-      res.render("index", { title: title, json: json });
+      // res.render("index", { title: title, json: json });
+      res.json({ title: title, json: json });
     })();
   } catch (error) {
     console.log(error);
@@ -24,6 +26,18 @@ app.get("/db", (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.post("/add", (req, res) => {
+  (async () => {
+    var API = await notionjs.getAPI();
+    await notionjs.addDataBlock(
+      process.env.blockID,
+      moment().format("MMMM Do YYYY, h:mm:ss a"),
+      JSON.stringify(API)
+    );
+    res.json(API);
+  })();
 });
 
 app.listen(process.env.PORT);
